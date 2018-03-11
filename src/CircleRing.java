877851f700086baed3a04febba07b1894c65d729
DylanArnold101 <league@WTS-IM-17.attlocal.net>
2018-03-11 11:29:20 -0700

    3 recipes
    da

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,26 +9,40 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+       Robot arnoldSwchaunz =  new Robot(); 
 
        //  2. Set your robot’s position to x=150 and y=200
-
+       		arnoldSwchaunz.setX(150);
+       		arnoldSwchaunz.setY(200);
         // 3. Put the robot's pen down
-
+       		arnoldSwchaunz.penDown();
         // 4. Set the robot’s speed to 10
-
+       		arnoldSwchaunz.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
-
+       		for (int i = 0; i <360; i++) {
+				
+			
 	        // 6. Move the robot 3 pixels
-
+       		arnoldSwchaunz.move(3);
        //  7. Turn the robot 1 degree
-
+       		arnoldSwchaunz.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+       		if(i%20 == 0) {
+       			for (int j = 0; j < 360; j++) {
+					arnoldSwchaunz.move(1);
+					arnoldSwchaunz.turn(1);
+       			}
+       			
+       			
+       			
+       			
+       			
+       		}
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
 				// 10. Move the robot 1 pixel
 
 				// 11. Turn the robot 1 degree
+}
 }
 }
